b451d87f6d7c7409e9e6570429a0ec9f5b1999ca
Rafael Bastos <47896406+rmpbastos@users.noreply.github.com>
2024-10-03 20:44:31 -0700

    Added src/context/UserContext.js

---
+++ client/src/context/UserContext.js
@@ -0,0 +1,31 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebaseConfig"
+
+const UserContext = createContext();
+
+export const useAuth = () => useContext(UserContext);
+
+export const AuthProvider = ({ children }) => {
+    const[currentUser, setCurrentUser] = useState(null);
+    const[loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setCurrentUser(user);
+            setLoading(false);
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    if (loading) {
+        return <p>Loading...</p>
+    }
+
+    return (
+        <UserContext.Provider value={{ currentUser }}>
+            { children }
+        </UserContext.Provider>
+    );
+};
